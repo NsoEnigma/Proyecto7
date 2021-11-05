171e8eb033b3e478e5bff31b9716e0dc665860a1
Alejandro Asencio Montes <63419024+NsoEnigma@users.noreply.github.com>
2021-11-05 13:58:09 +0100

    inicio

--- src/main/java/paquete/Proyecto7.java
+++ src/main/java/paquete/Proyecto7.java
@@ -1,5 +1,22 @@
 package paquete;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoField;
+import java.util.Scanner;
+
 public class Proyecto7 {
 
+	public static void main(String[] args) {
+		Scanner lector = new Scanner(System.in);
+		
+		
+	
+	
+	
+	
+	
+	}
 }
